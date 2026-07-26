be027e886da2334085b74335feffe9024515e805
Naveed Abdalkhani <73241694+Squigglbunny@users.noreply.github.com>
2026-07-26 15:09:24 -0700

    Add custom SVG icon for 'pray' to ribbon

--- src/main.ts
+++ src/main.ts
@@ -20,8 +20,10 @@ export default class MyPlugin extends Plugin {
 	async onload() {
 		await this.loadSettings();
 
+		// add custom svg icon
+		addIcon('pray', '<path d="M2 15v-5c0-1.1.5-2 1.5-3l3.09-3.25c1.059-1.12 2.932-.674 3.373.802A2 2 0 019.5 6.5l-3.4 4.2L6 14v-2c0-1.54 1.667-2.502 3-1.732A2 2 0 0110 12v3c0 .6-.2 1.1-.6 1.4l-4.9 5.1" /> <path d="M22 15v-5c0-1.1-.5-2-1.5-3l-3.09-3.25c-1.059-1.12-2.932-.674-3.373.802A2 2 0 0014.5 6.5l3.4 4.2.1 3.3v-2c0-1.54-1.667-2.502-3-1.732A2 2 0 0014 12v3c0 .6.2 1.1.6 1.4l4.9 5.1" />`)
 		// This creates an icon in the left ribbon.
-		this.addRibbonIcon('sun', 'Sample', (_evt: MouseEvent) => {
+		this.addRibbonIcon('pray', 'Sample', (_evt: MouseEvent) => {
 			// Called when the user clicks the icon.
 			new Notice('This is a notice!');
 		});
